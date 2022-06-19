6afedfc850481658e5ca62f01deddaf04d6655ab
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-06-19 11:28:15 +0200

    update index.ts and create new scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { ApolloServer } from 'apollo-server';
 import { context } from './context';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
-// 1
 import { schema } from './schema';
 export const server = new ApolloServer({
   schema,
   context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
-const port = 3000;
-// 2
+const port = process.env.PORT || 3000;
 server.listen({ port }).then(({ url }) => {
   console.log(`🟢 Server ready at ${url}`);
 });
