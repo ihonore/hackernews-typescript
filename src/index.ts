e55f9c46d9d9582e424e29f3b98c366f75cfa258
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-06-19 12:45:39 +0200

    changed graphql version

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export const server = new ApolloServer({
   plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 server.listen({ port }).then(({ url }) => {
   console.log(`🟢 Server ready at ${url}`);
 });
